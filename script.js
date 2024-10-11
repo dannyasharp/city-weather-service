4db9d7c16e8a6a22a31b9c512338efc8d064cca4
dannyasharp <dannyasharp@gmail.com>
2024-10-10 22:13:28 -0700

    gather officeID data, headlines, dates, and news links

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 
 const weatherButton = document.getElementById("getWeather")
 const dropdown = document.getElementById("dropdown")
+const headlinesContainer = document.getElementById("headlinesContainer");
+const headerResults = document.getElementById("headerResults");
 
 weatherButton.addEventListener('click', function() {
     const cityID = dropdown.value;
     fetchHeadlines(cityID);
-    showCityIdImage(cityID);
 });
 
+// fetch city data depending on city
 function fetchHeadlines(cityID) {
     const url = `https://api.weather.gov/offices/${cityID}/headlines`;
 
@@ -27,11 +29,37 @@ function fetchHeadlines(cityID) {
         displayHeadlines(data)
     })
     .catch(error => {
-        console.log("error fetching headlines", error);
+        console.error("Error fetching headlines:", error);
     })
 
 };
 
-function showCityIdImage() {
+function displayHeadlines(data) {
+    headlinesContainer.innerHTML = '';  // clear the headlines
 
-};
+    //check for data, and @graph field and it's content
+    if(data && data['@graph'] && data['@graph'].length > 0) {
+        data['@graph'].forEach(headline => {
+            const card = document.createElement('div');
+            card.className = 'headlineCard';
+
+        // make the headline with a title, date, and link
+        const title = headline.title || 'No title available';
+        const date = new Date(headline.issuanceTime).toLocaleString() || 'No data available';
+        const link = `<a href=${headline.link} target="_blank"> Link to News </a>` || 'No News Link Available';
+
+        card.innerHTML = `
+        <h3>${title}</h3>
+        <p><strong>Issued on:</strong> ${date}</p>
+        <p>${link} </p>
+        `;
+
+        headlinesContainer.appendChild(card);
+        });
+
+    } else {
+        headlinesContainer.innerHTML = '<h3>No recent headlines found for this city.</h3>'
+    }
+
+    headerResults.classList.remove('hidden');
+}
